dfca3350c0188feaeea5babf693932b10c0c122d
Marvin Preuss <marvin@xsteadfastx.org>
2021-10-05 10:24:36 +0200

    concurrent key loading

--- ssh.gopass.sh
+++ ssh.gopass.sh
@@ -2,9 +2,12 @@
 set -e
 
 if command -v gopass >>/dev/null; then
+	eval "$(ssh-agent)"
+	trap "ssh-agent -k" EXIT
 	for key in $(gopass ls -f ssh/ | grep privkey); do
-		gopass show -n "$key" | ssh-add -
+		gopass show -n "$key" | ssh-add - &
 	done
 fi
 
+wait
 /usr/bin/ssh "$@"
